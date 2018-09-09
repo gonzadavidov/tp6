30ccc80d29b193be225a9609cdffc157eebb9816
gonzalo Davidov <gdavidov@itba.edu.ar>
2018-09-09 15:00:21 -0300

    aa

---
+++ tp6/server/Server.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <boost/asio.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/chrono.hpp>
+#include <boost/timer/timer.hpp>
+
+
+#define HELLO_PORT "80"
+
+
+class server
+{
+public:
+	server();
+	void startConnection();
+	void sendMessage();
+	void receiveMessage();
+	~server();
+
+private:
+	boost::asio::io_service*  IO_handler;
+	boost::asio::ip::tcp::socket* socket_forServer;
+	boost::asio::ip::tcp::acceptor* server_acceptor;
+	bool CLRF();
+};
