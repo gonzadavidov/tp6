332e8d50908930e70e9d176e82e0b6832c681381
Niko Villanueva <nikovicio@hotmail.com>
2018-09-09 15:42:50 -0300

    hola

---
+++ tp6/client/main.cpp
@@ -0,0 +1,30 @@
+#include "client.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct mensaje
+{
+	char* inicio;
+}; 
+
+}
+
+int
+main(int argc, char* argv[])
+{
+	client cliente;
+	cout << "Trying to connect to " << SERVER_IP << " on port " << HELLO_PORT_STR << endl;
+	cliente.startConnection(SERVER_IP);
+	cliente.receiveMessage();
+	
+	ofstream archivo_salida("file.txt");    // Se abre el archivo para escribir.
+
+	archivo_salida << mensaje.inicio << endl;     // Se escribe en el archivo
+	archivo_salida.close();                     // SE cierra el archivo
+
+	cout << "Press Enter to exit..." << endl;
+	getchar();
+	return 0;
+}
