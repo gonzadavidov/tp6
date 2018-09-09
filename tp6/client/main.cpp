24376e434011d2a5a8fdca6ab8bfd2292a7e4bc4
Niko Villanueva <nikovicio@hotmail.com>
2018-09-09 16:33:03 -0300

    hola3

--- tp6/client/main.cpp
+++ tp6/client/main.cpp
@@ -4,13 +4,15 @@
 
 using namespace std;
 
+char*  obtenerHost(int argc, char* argv, char host[]);
 
 int
 main(int argc, char* argv[])
 {
 	client cliente;
+	char host[500];
 	cout << "Trying to connect to " << SERVER_IP << " on port " << HELLO_PORT_STR << endl;
-	cliente.startConnection(SERVER_IP);
+	cliente.startConnection(obtenerHost(argc,argv,host));
 	cliente.receiveMessage();
 	
 	ofstream archivo_salida("file.txt");			// Se abre el archivo para escribir
@@ -20,4 +22,13 @@ main(int argc, char* argv[])
 	cout << "Press Enter to exit..." << endl;
 	getchar();
 	return 0;
+}
+
+char*  obtenerHost(int argc, char* argv, char host[]) {
+	int i = 0;
+	while (*(argv[i]) != "/") {
+		host[i] = argv[i];
+		i++;
+	}
+	return host;
 }
